6af7c75758006c0cac72de3a2bd651760f823d67
fayddelight <66011598+fayddelight@users.noreply.github.com>
2022-04-14 09:17:54 +0200

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Elixir Jobs changelog
+
+## v2.1
+
+- Updated to Elixir 1.9.1 and Erlang 22.0
+- Update Ecto and Phoenix
+- Update DateTime helper functions
+
+## v2.0
+
+- Removed Markdown and long descriptions
+- Updated to Elixir 1.6.4 and Erlang 20.3.2
+- Add formatter
+- Add Credo
+- UI redesign
+
+## v1.0
+
+- Initial version
+- Email alerts of new job postings
+- Send published jobs to Twitter
---
+++ mix.exs
@@ -0,0 +1,84 @@
+defmodule ElixirJobs.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_jobs,
+      version: "0.0.1",
+      elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {ElixirJobs.Application, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      # Phoenix
+      {:phoenix, "~> 1.5.0"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:plug_cowboy, "~> 2.1"},
+      {:phoenix_html, "~> 3.0"},
+      {:gettext, "~> 0.18.2"},
+      # Database
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.7"},
+      {:postgrex, ">= 0.0.0"},
+      # Auth
+      {:guardian, "~> 2.2"},
+      {:comeonin, "~> 5.3"},
+      {:bcrypt_elixir, "~> 2.3"},
+      # External services
+      {:extwitter, "~> 0.12.4"},
+      {:bamboo, "~> 2.0"},
+      {:bamboo_phoenix, "~> 1.0"},
+      {:nadia, "~> 0.7"},
+      # Monitoring
+      {:appsignal_phoenix, "~> 2.0"},
+      # Misc
+      {:slugger, "~> 0.3.0"},
+      {:scrivener_ecto, "~> 2.7"},
+      {:html_sanitize_ex, "~> 1.4"},
+      {:calendar, "~> 1.0"},
+      {:jason, "~> 1.2"},
+      # Tests and dev
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:faker, "~> 0.16", only: :test},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate", "test"]
+    ]
+  end
+end
